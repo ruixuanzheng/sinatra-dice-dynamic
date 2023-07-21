f5b0aa4dd1f46e61646c7f5b649269ce7ee9ee3f
ruixuanzheng <96707192+ruixuanzheng@users.noreply.github.com>
2023-07-21 01:01:53 +0000

    2nd

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dynamic/:number_of_dice/:how_many_sides") do
+get("/dice/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
 
   @sides = params.fetch("how_many_sides").to_i
